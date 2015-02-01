546ef828cfefb70b8f97ae86f27bcacc93bca2e6
Shah <shahnewazk@gmail.com>
2015-01-31 19:22:22 -0800

    Added header to home.php
    Signed-off-by: Shah <shahnewazk@gmail.com>

--- application/views/home.php
+++ application/views/home.php
@@ -1,15 +1,58 @@
 <!doctype html>
 <html class="no-js" lang="en">
   <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Bookie Wookie | Welcome</title>
-    <link rel="stylesheet" href="assets/foundation/css/foundation.css" />
-    <link rel="stylesheet" href="assets/foundation/css/app.css">
-    <script src="assets/foundation/js/vendor/modernizr.js"></script>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Bookie Wookie | Welcome</title>
+	<link rel="stylesheet" href="assets/foundation/css/foundation.css" />
+	<link rel="stylesheet" href="assets/foundation/css/app.css">
+	<script src="assets/foundation/js/vendor/modernizr.js"></script>
   </head>
   <body>
-    
+    <!-- begin nav -->
+
+    <div class="contain-to-grid sticky"> 
+	    <nav class="top-bar" data-topbar role="navigation"> 
+	    	<ul class="title-area"> 
+	    		<li class="name"> 
+	    			<h1><a href="#">Bookie Wookie</a></h1> 
+	    		</li> 
+	    		<!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone --> 
+	    		<li class="toggle-topbar menu-icon">
+	    			<a href="#"><span>Menu</span></a>
+	    		</li> 
+	    	</ul> 
+	    	<section class="top-bar-section"> 
+		    	<!-- Right Nav Section --> 
+		    	<ul class="right"> 
+		    		<li class="active">
+		    			<a href="#">Right Button Active</a>
+		    		</li> 
+		    		<li class="has-dropdown"> 
+
+		    			<a href="#">Right Button Dropdown</a> 
+
+		    			<ul class="dropdown"> 
+		    				<li>
+		    					<a href="#">First link in dropdown</a>
+		    				</li> 
+		    				<li class="active">
+		    					<a href="#">Active link in dropdown</a>
+		    				</li> 
+		    			</ul> 
+		    		</li> 
+		    	</ul> 
+		    	<!-- Left Nav Section --> 
+		    	<ul class="left"> 
+		    		<li>
+		    			<a href="#">Left Nav Button</a>
+		    		</li> 
+		    	</ul> 
+		    </section> 
+	    </nav>   
+    </div>
+    <!-- end nav -->
+  	
   	<!-- begin paralax -->              
 	<div class="parallax-background">
 	  <div class="intro-text">
